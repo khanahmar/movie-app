477c2c934dea0e4f8f8d1f4cd7572ef9685d8db8
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-15 12:43:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=08dd3510b22d86fbdd6c6e0a3b59b341&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/discover/movie?api_key=08dd3510b22d86fbdd6c6e0a3b59b341&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+// get initials movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
